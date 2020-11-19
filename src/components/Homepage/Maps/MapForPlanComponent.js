cd4bf81934d2cd6e3ff75e1272b9dd9b6ec399aa
yutinggan <gan.yut@husky.neu.edu>
2020-11-18 23:41:00 -0500

    delete import mapStyle

--- src/components/Homepage/Maps/MapForPlanComponent.js
+++ src/components/Homepage/Maps/MapForPlanComponent.js
@@ -21,7 +21,6 @@ import CardComponent from "./CardComponent";
 import MapDirectionsRenderer from "./MapDirectionComponent";
 import SearchBar from "../SearchBarComponent";
 
-import MapStyle from "./MapStyle";
 import {Combobox, ComboboxInput, ComboboxList, ComboboxOption, ComboboxPopover} from "@reach/combobox";
 
 // to avoid rerender
