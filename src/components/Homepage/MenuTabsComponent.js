d8d7deb2c17566c011c51bb063d82f86053701fc
yutinggan <gan.yut@husky.neu.edu>
2020-11-19 16:16:07 -0500

    fix homepage icon

--- src/components/Homepage/MenuTabsComponent.js
+++ src/components/Homepage/MenuTabsComponent.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { FaPencilAlt, FaCalendarPlus } from 'react-icons/fa';
+import { FaPencilAlt, FaCalendarPlus, FaWpforms, FaSearchLocation } from 'react-icons/fa';
 
 export default function MenuTabs(){
     return(
         <nav className="nav nav-pills nav-justified">
             <a className="nav-item nav-link pill-item" href="/">
-                <i className="fas fa-search-location"></i>
+                <FaSearchLocation/>
                 Search places
             </a> 
             <a className="nav-item nav-link pill-item"
                href="/posts/details">
-                <i className="fab fa-wpforms"></i>
+                <FaWpforms />
                     Travel Forums
             </a>  
             <a className="nav-item nav-link pill-item" href="/posts"><FaPencilAlt />Write a post</a>
