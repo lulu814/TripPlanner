ca59bae468f5e1fb64177ff85192807867f9fa77
changsu <suchang177@outlook.com>
2020-11-26 10:07:14 -0500

    moogo init

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/TripPlanner',
+    {useNewUrlParser: true});
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
