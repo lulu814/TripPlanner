9521cacb9bcfd389d094b0d84857aad6f7d63360
lulu814 <59003045+lulu814@users.noreply.github.com>
2020-12-01 01:09:47 -0500

    Update users-service.js

---
+++ backend/services/users-service.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/users/user.model");
+const config = require("../config/auth.config")
+
+const checkDuplicateEmail = (req, res, next) => {
+    // register verify
+    User.findOne({
+        email: req.body.email
+    }).exec((err, user) => {
+        if (err) {
+            res.status(500).send({message: err});
+            return;
+        }
+
+        if (user) {
+            res.status(400).send({message: "Failed! Email is already in use!"});
+            return;
+        }
+
+        next();
+    });
+};
+
+const verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+    console.log("token ->", token);
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        next();
+    });
+};
+
+
+module.exports = {checkDuplicateEmail, verifyToken}
