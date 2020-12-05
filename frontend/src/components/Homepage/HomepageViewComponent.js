aca3827a892bb14234ecc9304f30c75247109c36
yutinggan <gan.yut@husky.neu.edu>
2020-12-05 10:37:40 -0500

    moved function

--- frontend/src/components/Homepage/HomepageViewComponent.js
+++ frontend/src/components/Homepage/HomepageViewComponent.js
@@ -3,20 +3,14 @@ import {Container} from "react-bootstrap";
 import PlaceList from "./Place/PlaceListComponent";
 import MenuTabs from "./MenuTabsComponent";
 import SearchBarComponent from "./PlainSearchBarComponent";
-import { useHistory } from "react-router";
+
 
 export default function HomepageView(){
-    const history = useHistory();
-    const updateSearchText = (text) => {
-        console.log(`setting address to: ${text}`);
-        history.push(`/search/${text}`);
-    }
-
     return(
         <Container>
             <MenuTabs/>
             <Container fluid style={{height: '100vh'}}>
-                <SearchBarComponent updateText={updateSearchText} />
+                <SearchBarComponent />
                 <PlaceList/>
             </Container>
         </Container>
--- frontend/src/components/Homepage/PlainSearchBarComponent.js
+++ frontend/src/components/Homepage/PlainSearchBarComponent.js
@@ -1,7 +1,14 @@
 import React, {useState} from "react";
+import { useHistory } from "react-router";
 
 export default function SearchBar({updateText}) {
     const [searchText, setSearchText] = useState('');
+
+    const history = useHistory();
+    const updateSearchText = (text) => {
+        console.log(`setting address to: ${text}`);
+        history.push(`/search/${text}`);
+    }
 
     return(
         <div className='search-section'>
@@ -17,7 +24,7 @@ export default function SearchBar({updateText}) {
                             required/>
                     </div>
                     <div className="td search-td" id="s-cover">
-                        <button type="submit" className="search-button" onClick={event => updateText(searchText)}>
+                        <button type="submit" className="search-button" onClick={event => updateSearchText(searchText)}>
                             <div id="s-circle"></div>
                             <span className="search-span-stick"></span>
                         </button>
