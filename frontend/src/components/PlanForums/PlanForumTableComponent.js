8147d84c1257a09717799e73d183fdde40409eb3
Tianle Dong <dtllawrence@gmail.com>
2020-12-08 13:56:39 -0500

    add comma between trip places

--- frontend/src/components/PlanForums/PlanForumTableComponent.js
+++ frontend/src/components/PlanForums/PlanForumTableComponent.js
@@ -5,21 +5,25 @@ import TripService from "../../services/TripService";
 class PlanForumTableComponent extends React.Component {
 
     state = {
-        trips: []
+        trips: [],
+        isLoaded: false
     }
 
     componentDidMount() {
         TripService.findTripsForPlan(this.props.planId)
             .then(fetchedTrips => this.setState({trips: fetchedTrips}))
+            .then(r => this.setState({isLoaded: false}))
+            .then(r => this.setState({isLoaded: true}))
     }
 
     render() {
         return <div className="mt-4">
             <div className="wbdv-td-table-body-container">
                 <ol className="wbdv-td-list">
-                    {this.state.trips &&
+
+                    {this.state.isLoaded &&
                      this.state.trips.map(
-                        trip => <PlanForumTableItemComponent key={trip._id} trip={trip}/>)
+                        trip => <PlanForumTableItemComponent size={this.state.trips.length} key={trip._id} trip={trip}/>)
                     }
                 </ol>
             </div>
--- frontend/src/components/PlanForums/PlanForumTableItemComponent.js
+++ frontend/src/components/PlanForums/PlanForumTableItemComponent.js
@@ -2,16 +2,17 @@ import React, {Component} from "react";
 
 class PlanForumTableItemComponent extends Component {
     state = {
-        trip: this.props.trip
+        trip: this.props.trip,
+        tripSize: this.props.size
     }
 
     render() {
         return <li className="wbdv-td-item text-center">
-            <h4 className="wbdv-td-headline">
-                {this.state.trip.places.map(
-                    (place, index) =>
-                        <span
-                            key={index}>{place}{'\n'}</span>)}</h4>
+            <h4 className="wbdv-td-headline">{<span>{this.state.trip.places &&
+                                                     this.state.trip.places.map(
+                                                         (place, index) =>
+                                                             <span key={index}> {place}{index !== this.state.tripSize && ','}</span>
+                                                     )}</span>}</h4>
 
             <span>{this.state.trip.date}</span>
             <hr/>
