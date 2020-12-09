51fc2a22f957fbd9cd62074dab6d453bfd8bb375
yutinggan <gan.yut@husky.neu.edu>
2020-12-09 17:52:01 -0500

    build

---
+++ build/precache-manifest.8275f62631fa71a07a1159f544f9e3b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b123b5b5289477f0030725f83e6e98a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27e7a53d6a6367d1092e",
+    "url": "/static/css/2.37473bbd.chunk.css"
+  },
+  {
+    "revision": "200bf907fd6967ef0181",
+    "url": "/static/css/main.23125e55.chunk.css"
+  },
+  {
+    "revision": "27e7a53d6a6367d1092e",
+    "url": "/static/js/2.d8da8279.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.d8da8279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200bf907fd6967ef0181",
+    "url": "/static/js/main.332b8107.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "e6fbc5cb543b90d435d21dc2ad28dcc8",
+    "url": "/static/media/avatar.e6fbc5cb.jpg"
+  },
+  {
+    "revision": "cce46cf7eec67f1f777fc461eddbaac2",
+    "url": "/static/media/illustration-2.cce46cf7.jpg"
+  },
+  {
+    "revision": "6b522b725837d5c61580f766a46ed863",
+    "url": "/static/media/illustration-3.6b522b72.jpg"
+  },
+  {
+    "revision": "d1bb138808707c49d0ecb64ade05ae36",
+    "url": "/static/media/illustration-4.d1bb1388.jpg"
+  },
+  {
+    "revision": "1a66193dd532c60c87dc9c09e65530cb",
+    "url": "/static/media/peach-bg.1a66193d.jpg"
+  },
+  {
+    "revision": "774d0b68f831fade74b69568e5bdcf22",
+    "url": "/static/media/peach-watercolor-texture-background.774d0b68.jpg"
+  },
+  {
+    "revision": "80529efa4eac44d81dd0ebb4a49e18be",
+    "url": "/static/media/wallpaper.80529efa.jpg"
+  }
+]);
