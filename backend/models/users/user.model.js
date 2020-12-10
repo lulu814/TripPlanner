e43b64f9a36edd8b1e26f7cc67b2e49231f91f21
lulu814 <59003045+lulu814@users.noreply.github.com>
2020-12-09 23:07:49 -0500

    Update user.model.js

--- backend/models/users/user.model.js
+++ backend/models/users/user.model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
     "User",
     new mongoose.Schema({
-        fName: String,
-        lName: String,
+        username: String,
         email: String,
         password: String,
         text: String,
